0821ac3e633ab0e0bc203fe69816de4d2f78f716
jeszdav <jeszdav@gmail.com>
2021-02-24 14:49:42 +0100

    center bg image

---
+++ main.js
@@ -0,0 +1,50 @@
+/* OLD PROMISE WAY */
+/* fetch("https://dog.ceo/api/breeds/list/all")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  }); */
+
+let timer;
+let deleteFirstPhotoDelay;
+
+/*   NEW PROMISE WAY */
+async function start() {
+  const response = await fetch("https://dog.ceo/api/breeds/list/all");
+  const data = await response.json();
+  createBreedList(data.message);
+}
+
+start();
+
+function createBreedList(breedList) {
+  document.getElementById("breed").innerHTML = `
+  <select onchange="loadByBreed(this.value)">
+        <option>Choose a dog breed</option>
+        ${Object.keys(breedList)
+          .map(function (breed) {
+            return `<option>${breed}</option>`;
+          })
+          .join("")} 
+  </select>
+  `;
+}
+
+async function loadByBreed(breed) {
+  if (breed != "Choose a dog breed") {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images
+    `);
+    const data = await response.json();
+    createSlideShow(data.message);
+  }
+}
+
+function createSlideShow(images) {
+  document.getElementById("slideshow").innerHTML = `
+  <div class="slide" style="background-image: url(
+    '${images[0]}'
+  )">
+  `;
+}
